c493717fd008716ab6202c48f3b117e27827619b
Dhruv Gajera <dhruvgajera05@gmail.com>
2024-11-27 17:45:25 +0530

    Changes

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -25,7 +25,7 @@ router.get("/api/me", JWTMiddleware.JWTMiddleware, profileController.meAPI);
 router.get("/api/address", JWTMiddleware.JWTMiddleware, profileController.addressAPI);
 
 router.post("/api/address", JWTMiddleware.JWTMiddleware, profileController.validateAddress, profileController.postAddressAPI);
-router.post("/api/editProfile/:id", JWTMiddleware.JWTMiddleware, imageHelper.uploadUserImage, profileController.updateProfileAPI);
+router.post("/api/editProfile", JWTMiddleware.JWTMiddleware, imageHelper.uploadUserImage, profileController.updateProfileAPI);
 
 router.delete("/api/delete/address/:id", JWTMiddleware.JWTMiddleware, profileController.deleteAddressAPI);
 
